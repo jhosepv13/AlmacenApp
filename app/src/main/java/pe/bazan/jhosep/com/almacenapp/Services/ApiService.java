56f4a417e895815854c52a2ee8cdcfbc0e60ac5c
Jhosep Bazan <jhosep.bazan@tecsup.edu.pe>
2018-05-08 06:46:13 -0500

    Tercer cambio

--- app/src/main/java/pe/bazan/jhosep/com/almacenapp/Services/ApiService.java
+++ app/src/main/java/pe/bazan/jhosep/com/almacenapp/Services/ApiService.java
@@ -7,12 +7,14 @@ import okhttp3.RequestBody;
 import pe.bazan.jhosep.com.almacenapp.Models.Producto;
 import pe.bazan.jhosep.com.almacenapp.Models.ResponseMessage;
 import retrofit2.Call;
+import retrofit2.http.DELETE;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.Multipart;
 import retrofit2.http.POST;
 import retrofit2.http.Part;
+import retrofit2.http.Path;
 
 /**
  * Created by Alumno on 4/05/2018.
@@ -37,5 +39,9 @@ public interface ApiService {
             @Part("detalles") RequestBody detalles,
             @Part MultipartBody.Part imagen
     );
+
+    @DELETE("/api/v1/productos/{id}")
+    Call<ResponseMessage> destroyProducto(@Path("id") Integer id);
+
 
 }
